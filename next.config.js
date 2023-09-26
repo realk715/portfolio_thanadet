b1acf6c780b17d4970eaea546bd4d3cbf9953161
realkk715 <realkk715@gmail.com>
2023-09-26 22:40:25 +0700

    for fleek deploy

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
+module.exports = {
+  exportTrailingSlash: true,
   experimental: {
     serverActions: true,
   },
 };
 
-module.exports = nextConfig;
+
